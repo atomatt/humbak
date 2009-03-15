d6262a30dbae92de2383e2714c05d7add2531131
Matt Goodall <matt@jdi-associates.com>
2009-03-15 12:36:21 +0000

    Improve test script to send files to server.
    There's actually simple DAV and Request classes now (although there's
    really nothing DAV-specific about them yet) to make it easier to stream
    data in either direction.
    
    Files are uploaded in smallish blocks and measures the approximate
    bandwidth. However, it doesn't try to conrol the bandwidth in any way
    yet.

--- sandbox/test.py
+++ sandbox/test.py
@@ -1,29 +1,114 @@
 import base64
 import httplib
 import sys
+import time
+import os.path
+import urlparse
+import urllib
 
-username, password = sys.argv[1:]
-auth = base64.b64encode(':'.join([username, password]))
 
-conn = httplib.HTTPSConnection('dav.humyo.com')
+CONN_FACTORY = {'http': httplib.HTTPConnection,
+                'https': httplib.HTTPSConnection}
 
-conn.connect()
-try:
-    conn.putrequest('PROPFIND', '/')
+
+BLOCK_SIZE = 1024*100
+
+
+class DAV(object):
+
+    def __init__(self, connargs):
+        self.connargs = connargs
+        self.conn = CONN_FACTORY[self.connargs['scheme']](self.connargs['hostname'], self.connargs.get('port'))
+
+    @classmethod
+    def from_url(cls, url):
+        return cls(parse_url(url))
+
+    def put_file(self, path, filename):
+        content_length = os.path.getsize(filename)
+        f = open(filename, 'rb')
+        try:
+            request = self._request('PUT', path, [('Content-Length', str(content_length))])
+            blocks_sent = 0
+            bytes_sent = 0
+            start_time = time.time()
+            while True:
+                data = f.read(BLOCK_SIZE)
+                if not data:
+                    break
+                request.send(data)
+                end_time = time.time()
+                blocks_sent += 1
+                bytes_sent += len(data)
+                bandwidth = float(bytes_sent) / (end_time-start_time)
+                print "written: %d (%f bytes/s)" % (bytes_sent, bandwidth)
+            return request.getresponse()
+        finally:
+            f.close()
+
+    def _request(self, method, path, headers=None):
+        fullpath = os.path.join(self.connargs['path'], path)
+        self.conn.putrequest(method, fullpath)
+        request = Request(self.conn)
+        auth = base64.b64encode(':'.join([self.connargs['username'],
+                                          self.connargs['password']]))
+        request.putheader('Authorization', 'Basic %s'%auth)
+        for name, value in headers:
+            request.putheader(name, value)
+        request.endheaders()
+        return request
+
+
+class Request(object):
+
+    def __init__(self, conn):
+        self.conn = conn
+
+    def putheader(self, name, value):
+        self.conn.putheader(name, value)
+
+    def endheaders(self):
+        self.conn.endheaders()
+
+    def send(self, data):
+        self.conn.send(data)
+
+    def getresponse(self):
+        return self.conn.getresponse()
+
+
+def parse_url(url):
+    spliturl = urlparse.urlsplit(url)
+    return {'scheme': spliturl.scheme,
+            'hostname': spliturl.hostname,
+            'port': spliturl.port,
+            'path': spliturl.path or '/',
+            'username': urllib.unquote(spliturl.username) if spliturl.username else None,
+            'password': urllib.unquote(spliturl.password) if spliturl.password else None}
+
+
+def list_collection(conn, path):
+    conn.putrequest('PROPFIND', path)
     conn.putheader('Depth', '1')
     conn.putheader('Authorization', 'Basic %s'%auth)
     conn.endheaders()
     response = conn.getresponse()
     print response.status, response.getheaders()
     print response.read()
-    print
-    conn.putrequest('PROPFIND', '/')
-    conn.putheader('Depth', '1')
-    conn.putheader('Authorization', 'Basic %s'%auth)
-    conn.endheaders()
-    response = conn.getresponse()
-    print response.status, response.getheaders()
-    print response.read()
-finally:
-    conn.close()
+
+
+if __name__ == '__main__':
+    import sys
+    url = sys.argv[1]
+    filenames = sys.argv[2:]
+    dav = DAV.from_url(url)
+    for filename in filenames:
+        print "Putting", filename, "..."
+        response = dav.put_file(os.path.join('/test', urllib.quote_plus(os.path.basename(filename), safe='/')), filename)
+        if response.status == 501:
+            print response.status
+            print response.getheaders()
+            print response.read()
+        else:
+            response.read()
 
